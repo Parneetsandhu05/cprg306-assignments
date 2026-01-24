18f281310e5d9e9a3e68e88a66e72b732298c6a8
Parneet Kaur <your-email@example.com>
2026-01-23 19:43:20 -0700

    Update github link

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Your Name</p> 
+      <Link href="https://github.com/parneetsandhu05/cprg306-assignments">
+   My GitHub Repository</Link>
+    </div>
+  );
+}
