78e49f399ca53e22c649ecf2b88e21aac7e34850
Parneet Kaur <your-email@example.com>
2026-01-30 18:06:03 -0700

    Added link for week 3

--- app/page.js
+++ app/page.js
@@ -4,10 +4,12 @@ export default function Home() {
   return (
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <p>
+      <div>
         <Link href="/week-2">Week 2 Assignment</Link>
+      </div>
+      <div>
         <Link href="/week-3">Week 3 Assignment</Link>
-      </p>
+      </div>
     </main>
   );
 }
