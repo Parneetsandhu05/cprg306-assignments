e29fe540cd3b75071b239f074334c3b8389b6ec7
Parneet Kaur <your-email@example.com>
2026-02-13 16:42:41 -0700

    Complete Week 5 - Form with State

---
+++ app/week-5/new-item.js
@@ -0,0 +1,79 @@
+"use client"; // Required for useState
+
+import { useState } from "react";
+
+export default function NewItem() {
+  // Initialize State
+  const [name, setName] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  const [category, setCategory] = useState("produce");
+
+  // Handle Form Submission
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const item = { name, quantity, category };
+
+    console.log(item); // Requirement: Log to console
+    alert(`Added Item: ${name}, quantity: ${quantity}, category: ${category}`); // Requirement: Alert
+
+    // Reset State
+    setName("");
+    setQuantity(1);
+    setCategory("produce");
+  };
+
+  return (
+    <div className="flex justify-center w-full">
+      <form onSubmit={handleSubmit} className="p-2 m-4 bg-slate-900 text-black max-w-sm w-full">
+        <div className="mb-2">
+          <input
+            type="text"
+            placeholder="Item name"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            className="w-full mt-1 border-2 border-gray-300 p-2 rounded-lg font-sans"
+          />
+        </div>
+
+        <div className="flex justify-between">
+          <input
+            type="number"
+            min="1"
+            max="99"
+            required
+            value={quantity}
+            onChange={(e) => setQuantity(Number(e.target.value))}
+            className="w-20 ml-1 border-2 border-gray-300 p-2 rounded-lg font-sans"
+          />
+
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            className="ml-1 border-2 border-gray-300 p-2 rounded-lg font-sans"
+          >
+            <option value="produce">Produce</option>
+            <option value="dairy">Dairy</option>
+            <option value="bakery">Bakery</option>
+            <option value="meat">Meat</option>
+            <option value="frozen">Frozen Foods</option>
+            <option value="canned">Canned Goods</option>
+            <option value="dry">Dry Goods</option>
+            <option value="beverages">Beverages</option>
+            <option value="snacks">Snacks</option>
+            <option value="household">Household</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
+
+        <button
+          type="submit"
+          className="w-full mt-4 py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"
+        >
+          +
+        </button>
+      </form>
+    </div>
+  );
+}
